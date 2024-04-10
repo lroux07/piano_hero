1f2d890acb6e1422d1b43f2e9ab6b1b4f04fdb45
lroux07 <lroux07@edenschool.fr>
2024-04-10 16:06:55 +0200

    Finish design

--- main.js
+++ main.js
@@ -1,116 +1,115 @@
 let container = document.querySelector('.container');
 let mappingBtn = document.querySelector('.mappingBtn');
+mappingBtn.classList.remove('active');
+
 
 container.style.display = 'none';
 
-
-// CREATE CLAVIER
-
-mappingBtn.addEventListener('click', () => {
-    container.style.display = 'flex';
-
-
-    for(let i = 0; i < 2; i++) {
-        let white = document.createElement('div');
-        white.classList.add('white');
-        container.appendChild(white);
-    
-        let black = document.createElement('div');
-        black.classList.add('black');
-        white.appendChild(black);
-    }
-    
+for(let i = 0; i < 2; i++) {
     let white = document.createElement('div');
     white.classList.add('white');
     container.appendChild(white);
     
-    for(let i = 0; i < 3; i++) {
-        let white = document.createElement('div');
-        white.classList.add('white');
-        container.appendChild(white);
+    let black = document.createElement('div');
+    black.classList.add('black');
+    white.appendChild(black);
+}
     
-        let black = document.createElement('div');
-        black.classList.add('black');
-        white.appendChild(black);
-    }
-    
-    white = document.createElement('div');
+let white = document.createElement('div');
+white.classList.add('white');
+container.appendChild(white);
+
+for(let i = 0; i < 3; i++) {
+    let white = document.createElement('div');
     white.classList.add('white');
     container.appendChild(white);
-    
-    
-    
-    for(let i = 0; i < 2; i++) {
-        let white = document.createElement('div');
-        white.classList.add('white');
-        container.appendChild(white);
-    
-        let black = document.createElement('div');
-        black.classList.add('black');
-        white.appendChild(black);
-    }
-    
-    white = document.createElement('div');
+
+    let black = document.createElement('div');
+    black.classList.add('black');
+    white.appendChild(black);
+}
+
+white = document.createElement('div');
+white.classList.add('white');
+container.appendChild(white);
+
+
+
+for(let i = 0; i < 2; i++) {
+    let white = document.createElement('div');
     white.classList.add('white');
     container.appendChild(white);
-    
-    for(let i = 0; i < 3; i++) {
-        let white = document.createElement('div');
-        white.classList.add('white');
-        container.appendChild(white);
-    
-        let black = document.createElement('div');
-        black.classList.add('black');
-        white.appendChild(black);
-    }
-    
-    white = document.createElement('div');
+
+    let black = document.createElement('div');
+    black.classList.add('black');
+    white.appendChild(black);
+}
+
+white = document.createElement('div');
+white.classList.add('white');
+container.appendChild(white);
+
+for(let i = 0; i < 3; i++) {
+    let white = document.createElement('div');
     white.classList.add('white');
     container.appendChild(white);
-    
-    
-    // CONFIG CLAVIER
-    
-    navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
-    
-    function onMIDISuccess(midiAccess) {
-        midiAccess.inputs.forEach((input) => {
-            // console.log(input)
-            input.onmidimessage = onMIDIMessage;
-        });
+
+    let black = document.createElement('div');
+    black.classList.add('black');
+    white.appendChild(black);
+}
+
+white = document.createElement('div');
+white.classList.add('white');
+container.appendChild(white);
+
+
+// CONFIG CLAVIER
+
+navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
+
+function onMIDISuccess(midiAccess) {
+    midiAccess.inputs.forEach((input) => {
+        // console.log(input)
+        input.onmidimessage = onMIDIMessage;
+    });
+}
+
+function onMIDIFailure(error) {
+    // console.log(error);
+}
+
+function onMIDIMessage(event) {
+    let str = `MIDI message received at timestamp ${event.timeStamp}[${event.data.length} bytes]: `;
+    for (const character of event.data) {
+      str += `0x${character.toString(16)} `;
     }
-    
-    function onMIDIFailure(error) {
-        // console.log(error);
+    console.log(str);
+}
+// CREATE CLAVIER
+
+
+let isMapping = false;
+
+let notes = [{
+    do: 'Do',
+    re: 'Re',
+    mi: 'Mi',
+    fa: 'Fa',
+    sol: 'Sol',
+    la: 'La',
+    si: 'Si'
+}];
+
+mappingBtn.addEventListener('click', () => {
+    container.style.display = 'flex';
+    mappingBtn.disabled = true;
+
+    isMapping = true;
+
+    function mapping() {
+        // white.classList.add = 'blue';
+        white.style.backgroundColor('blue');
     }
-    
-    function onMIDIMessage(event) {
-        let str = `MIDI message received at timestamp ${event.timeStamp}[${event.data.length} bytes]: `;
-        for (const character of event.data) {
-          str += `0x${character.toString(16)} `;
-        }
-        console.log(str);
-    
-        // let notes = [{
-        //     Do: "Do",
-        //     Re: "Re",
-        //     Mi: "Mi",
-        //     Fa: "Fa",
-        //     Sol: "Sol",
-        //     La: "La",
-        //     Si: "Si"
-        // }];
-    
-        // console.log(event)
-    
-        // str.addEventListener("gamepadconnected", (e) => {
-        //     console.log(
-        //       "Gamepad connected at index %d: %s. %d buttons, %d axes.",
-        //       e.gamepad.index,
-        //       e.gamepad.id,
-        //       e.gamepad.buttons.length,
-        //       e.gamepad.axes.length,
-        //     );
-        //   });
-    }
-})
+    mapping();
+});
