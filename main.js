f288f6397be97895221dc356312ea0900995ff4d
lroux07 <lroux07@edenschool.fr>
2024-04-10 09:48:24 +0200

    first commit - white buttons

---
+++ main.js
@@ -0,0 +1,7 @@
+let container = document.querySelector('.container')
+let size = 15
+
+for(let i = 0; i < size; i++) {
+    let touches = document.createElement('button');
+    container.appendChild(touches);
+}
