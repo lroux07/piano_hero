0ca1d0ee8ac5c785fc1cb8502bab930f72a945ce
lroux07 <lroux07@edenschool.fr>
2024-04-10 11:47:16 +0200

    Create clavier and connect midi

--- main.js
+++ main.js
@@ -1,7 +1,82 @@
-let container = document.querySelector('.container')
-let size = 15
+let container = document.querySelector('.container');
 
-for(let i = 0; i < size; i++) {
-    let touches = document.createElement('button');
-    container.appendChild(touches);
+for(let i = 0; i < 2; i++) {
+    let white = document.createElement('div');
+    white.classList.add('white');
+    container.appendChild(white);
+
+    let black = document.createElement('div');
+    black.classList.add('black');
+    white.appendChild(black);
 }
+
+let white = document.createElement('div');
+white.classList.add('white');
+container.appendChild(white);
+
+for(let i = 0; i < 3; i++) {
+    let white = document.createElement('div');
+    white.classList.add('white');
+    container.appendChild(white);
+
+    let black = document.createElement('div');
+    black.classList.add('black');
+    white.appendChild(black);
+}
+
+white = document.createElement('div');
+white.classList.add('white');
+container.appendChild(white);
+
+
+
+for(let i = 0; i < 2; i++) {
+    let white = document.createElement('div');
+    white.classList.add('white');
+    container.appendChild(white);
+
+    let black = document.createElement('div');
+    black.classList.add('black');
+    white.appendChild(black);
+}
+
+white = document.createElement('div');
+white.classList.add('white');
+container.appendChild(white);
+
+for(let i = 0; i < 3; i++) {
+    let white = document.createElement('div');
+    white.classList.add('white');
+    container.appendChild(white);
+
+    let black = document.createElement('div');
+    black.classList.add('black');
+    white.appendChild(black);
+}
+
+white = document.createElement('div');
+white.classList.add('white');
+container.appendChild(white);
+
+
+
+navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
+
+function onMIDISuccess(midiAccess) {
+    midiAccess.inputs.forEach((input) => {
+        console.log(input)
+        input.onmidimessage = onMIDIMessage;
+    });
+}
+
+function onMIDIFailure(error) {
+    // console.log(error);
+}
+
+function onMIDIMessage(event) {
+    let str = `MIDI message received at timestamp ${event.timeStamp}[${event.data.length} bytes]: `;
+    for (const character of event.data) {
+      str += `0x${character.toString(16)} `;
+    }
+    console.log(str);
+  }
